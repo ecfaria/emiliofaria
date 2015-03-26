59b607632e90307794e086d45b6cc0db83109163
Emilio Faria <emiliocfaria@gmail.com>
2015-03-26 00:30:48 -0300

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+// Include gulp
+var gulp = require('gulp'); 
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('app/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Minify our images
+gulp.task('images', function () {
+    return gulp.src('app/img/*')
+        .pipe(imagemin({
+            progressive: true,
+        }))
+        .pipe(gulp.dest('dist/img'));
+});
+
+// Compile Our Less
+gulp.task('less', function() {
+    return gulp.src('app/less/app.less')
+        .pipe(less())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(rename('main.min.css'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('dist/css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('app/js/*.js')
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(rename('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('app/img/*', ['images']);
+    gulp.watch('app/js/*.js', ['lint', 'scripts']);
+    gulp.watch('app/less/*.less', ['less']);
+});
+
+// Default Task
+gulp.task('default', ['images', 'lint', 'less', 'scripts', 'watch']);
